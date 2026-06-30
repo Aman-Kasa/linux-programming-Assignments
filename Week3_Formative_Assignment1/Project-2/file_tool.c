7725b978c5a00f5e0aede1856ba746902fae677c
Aman-Kasa <a.kasa@alustudent.com>
2026-06-30 22:29:33 +0200

    project 2

---
+++ Week3_Formative_Assignment1/Project-2/file_tool.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // 1. File Creation & Log Writing
+    FILE *fp = fopen("runtime_log.txt", "w");
+    if (fp == NULL) {
+        perror("Failed to create file");
+        return 1;
+    }
+    fprintf(fp, "Log Entry: System execution monitored successfully.\n");
+    fclose(fp);
+
+    // 2. Reading Files
+    fp = fopen("runtime_log.txt", "r");
+    if (fp == NULL) {
+        perror("Failed to open file for reading");
+        return 1;
+    }
+    char ch;
+    while ((ch = fgetc(fp)) != EOF) {
+        // Continuous reading until end of file
+    }
+    fclose(fp);
+
+    return 0;
+}
